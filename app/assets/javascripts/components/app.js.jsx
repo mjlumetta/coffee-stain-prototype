5c911cb875ffa8bfc0715a0d830a3f9f0299f2b3
Mike Lumetta <mjlumetta@gmail.com>
2016-11-03 23:18:28 -0400

    React and Jasmine setup

---
+++ app/assets/javascripts/components/app.js.jsx
@@ -0,0 +1,10 @@
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h1>Home</h1>
+				<p>Hello Coffee Stain!</p>
+			</div>
+		);
+	}
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 end
